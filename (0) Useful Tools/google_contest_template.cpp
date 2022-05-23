b85c07fdea4bcae2339265324c49bad9237efb4f
unknown <100839060+StephenC45@users.noreply.github.com>
2022-05-23 13:24:17 +1000

    Added C++ template for Google contests.

---
+++ (0) Useful Tools/google_contest_template.cpp
@@ -0,0 +1,32 @@
+/*
+A C++ template to use in Google Code Jam and Google Kick Start.
+*/
+
+
+
+#include <bits/stdc++.h>
+
+#define int long long
+
+using namespace std;
+
+
+
+int32_t main(void) {
+    // Read the number of test cases.
+    int test_count;
+    cin >> test_count;
+
+    // Process each test case.
+    for (int i = 1; i <= test_count; ++i) {
+        cout << endl;
+        // Write your code here.
+        
+
+        // Print the line of output for each test case. Be sure to update
+        // the "Your output here..." part.
+        cout << "Case #" << i << ": " << "Your output here..." << endl;
+    }
+
+    return 0;
+}
