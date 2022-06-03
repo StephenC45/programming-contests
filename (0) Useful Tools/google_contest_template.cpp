0e6264b61d49a0e75d50520ccf74dd586e68ce9d
unknown <100839060+StephenC45@users.noreply.github.com>
2022-06-03 19:57:29 +1000

    Made I/O faster in Google contest template.

--- (0) Useful Tools/google_contest_template.cpp
+++ (0) Useful Tools/google_contest_template.cpp
@@ -8,24 +8,27 @@ A C++ template to use in Google Code Jam and Google Kick Start.
 
 #define int long long
 
-using namespace std;
+using namespace std; // Please don't do this outside of programming contests...
 
 
 
 int32_t main(void) {
+    // Makes I/O faster, but you cannot mix cin/cout with scanf()/printf().
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
     // Read the number of test cases.
     int test_count;
     cin >> test_count;
 
     // Process each test case.
     for (int i = 1; i <= test_count; ++i) {
-        cout << endl;
         // Write your code here.
         
 
         // Print the line of output for each test case. Be sure to update
         // the "Your output here..." part.
-        cout << "Case #" << i << ": " << "Your output here..." << endl;
+        cout << "Case #" << i << ": " << "Your output here..." << '\n';
     }
 
     return 0;
